0b4a20bf57678eac0d057352bef860f046b968df
sowjanya <nandamsowjanya3@gamil.com>
2026-03-22 13:35:51 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sowjanya & Vrunda",
+    author_email="sowji.seelu14@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
